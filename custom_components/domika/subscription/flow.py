b5d6dfc1f1d17dca3c4b40713ecb8d171f94a3ad
Artem Bezborodko <artem.bezborodko@atisgroup.ru>
2024-05-11 14:17:16 +0300

    Added subscription flow and service.

---
+++ custom_components/domika/subscription/flow.py
@@ -0,0 +1,69 @@
+# vim: set fileencoding=utf-8
+"""
+Subscription data.
+
+(c) DevPocket, 2024
+
+
+Author(s): Artem Bezborodko
+"""
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .models import DomikaSubscriptionCreate, DomikaSubscriptionUpdate
+from .service import create, delete, update_in_place
+
+
+# TODO: maybe reorganize data so it can support schemas.
+async def resubscribe(
+    db_session: AsyncSession,
+    app_session_id: str,
+    subscriptions: dict[str, list[str]],
+):
+    """Remove all existing subscriptions, and subscribe to the new subscriptions."""
+    await delete(db_session, app_session_id, commit=False)
+    for entity, attrs in subscriptions.items():
+        for attr in attrs:
+            await create(
+                db_session,
+                DomikaSubscriptionCreate(
+                    app_session_id=app_session_id,
+                    entity_id=entity,
+                    attribute=attr,
+                    need_push=False,
+                ),
+                commit=False,
+            )
+    await db_session.commit()
+
+
+# TODO: maybe reorganize data so it can support schemas.
+async def resubscribe_push(
+    db_session: AsyncSession,
+    app_session_id: str,
+    subscriptions: dict[str, list[str]],
+):
+    """
+    Set need_push for given app_session_id.
+
+    Set need_push to true for given entities attributes, for all other set need_push to false.
+    """
+    await update_in_place(
+        db_session,
+        app_session_id,
+        entity_id='',
+        attribute='',
+        subscription_in=DomikaSubscriptionUpdate(need_push=False),
+        commit=False,
+    )
+    for entity, attrs in subscriptions.items():
+        for attr in attrs:
+            await update_in_place(
+                db_session,
+                app_session_id=app_session_id,
+                entity_id=entity,
+                attribute=attr,
+                subscription_in=DomikaSubscriptionUpdate(need_push=True),
+                commit=False,
+            )
+    await db_session.commit()
---
+++ custom_components/domika/subscription/service.py
@@ -0,0 +1,112 @@
+# vim: set fileencoding=utf-8
+"""
+Subscription data.
+
+(c) DevPocket, 2024
+
+
+Author(s): Artem Bezborodko
+"""
+
+from collections.abc import Sequence
+from dataclasses import asdict
+
+import sqlalchemy
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .models import DomikaSubscriptionCreate, DomikaSubscriptionUpdate, Subscription
+
+
+async def get(
+    db_session: AsyncSession,
+    app_session_id: str,
+    *,
+    need_push: bool = True,
+) -> Sequence[Subscription]:
+    """
+    Get all subscriptions by application sesison id.
+
+    Subscriptions filtered by need_push flag.
+    """
+    dev = sqlalchemy.select(Subscription).where(
+        Subscription.app_session_id == app_session_id,
+        Subscription.need_push == need_push,
+    )
+    return (await db_session.scalars(dev)).all()
+
+
+async def get_by_attributes(
+    db_session: AsyncSession,
+    entity_id: str,
+    attributes: list[str],
+) -> Sequence[Subscription]:
+    """Get all subscriptions with given entity_id that contains attribute from attributes."""
+    dev = sqlalchemy.select(Subscription).where(
+        Subscription.entity_id == entity_id,
+        Subscription.attribute.in_(attributes),
+    )
+    return (await db_session.scalars(dev)).all()
+
+
+async def create(
+    db_session: AsyncSession,
+    subscription_in: DomikaSubscriptionCreate,
+    *,
+    commit: bool = True,
+):
+    """Create new subscription."""
+    subscription = Subscription(**subscription_in.to_dict())
+    db_session.add(subscription)
+    await db_session.flush()
+    if commit:
+        await db_session.commit()
+
+
+async def update(
+    db_session: AsyncSession,
+    subscription: Subscription,
+    subscription_in: DomikaSubscriptionUpdate,
+    *,
+    commit: bool = True,
+):
+    """Update subscription."""
+    subscription_attrs = subscription.dict()
+    update_data = asdict(subscription_in)
+    for attr in subscription_attrs:
+        if attr in update_data:
+            setattr(subscription, attr, update_data[attr])
+
+    if commit:
+        await db_session.commit()
+
+
+async def update_in_place(
+    db_session: AsyncSession,
+    app_session_id: str,
+    entity_id: str,
+    attribute: str,
+    subscription_in: DomikaSubscriptionUpdate,
+    *,
+    commit: bool = True,
+):
+    """Update subscription in place."""
+    stmt = sqlalchemy.update(Subscription)
+    stmt = stmt.where(Subscription.app_session_id == app_session_id)
+    if entity_id:
+        stmt = stmt.where(Subscription.entity_id == entity_id)
+    if attribute:
+        stmt = stmt.where(Subscription.attribute == attribute)
+    stmt = stmt.values(**asdict(subscription_in))
+    await db_session.execute(stmt)
+
+    if commit:
+        await db_session.commit()
+
+
+async def delete(db_session: AsyncSession, app_session_id: str, *, commit: bool = True):
+    """Delete subscription."""
+    stmt = sqlalchemy.delete(Subscription).where(Subscription.app_session_id == app_session_id)
+    await db_session.execute(stmt)
+
+    if commit:
+        await db_session.commit()

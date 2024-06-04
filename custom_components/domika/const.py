1dfdf5e245159fb24701d72ba4ac2fc10cfbbc1d
Artem Bezborodko <artem.bezborodko@atisgroup.ru>
2024-06-05 01:29:03 +0300

    Removed redundant comments.

--- custom_components/domika/const.py
+++ custom_components/domika/const.py
@@ -8,19 +8,18 @@ from homeassistant.components.binary_sensor import BinarySensorDeviceClass
 
 LOGGER = logging.getLogger(__name__)
 
-DOMAIN = 'Domika'
+DOMAIN = "Domika"
 MAIN_LOGGER_NAME = DOMAIN
 
-# ALEMBIC_INI_PATH = 'config/custom_components/domika/alembic.ini'
-ALEMBIC_INI_PATH = 'custom_components/domika/alembic.ini'
+ALEMBIC_INI_PATH = "custom_components/domika/alembic.ini"
 
-if os.getenv('DOMIKA_DEBUG') == '1':
-    DATABASE_URL = os.getenv('DOMIKA_DATABASE_URL')
-    PUSH_SERVER_URL = os.getenv('DOMIKA_PUSH_SERVER_URL')
-    PUSH_INTERVAL = timedelta(seconds=int(os.getenv('DOMIKA_PUSH_INTERVAL') or 30))
+if os.getenv("DOMIKA_DEBUG") == "1":
+    DATABASE_URL = os.getenv("DOMIKA_DATABASE_URL")
+    PUSH_SERVER_URL = os.getenv("DOMIKA_PUSH_SERVER_URL")
+    PUSH_INTERVAL = timedelta(seconds=int(os.getenv("DOMIKA_PUSH_INTERVAL") or 30))
 else:
-    DATABASE_URL = 'sqlite+aiosqlite:///Domika.db'
-    PUSH_SERVER_URL = 'https://domika.app'
+    DATABASE_URL = "sqlite+aiosqlite:///Domika.db"
+    PUSH_SERVER_URL = "https://domika.app"
     PUSH_INTERVAL = timedelta(minutes=15)
 
 # Seconds
@@ -29,7 +28,7 @@ PUSH_SERVER_TIMEOUT = 10
 # Number of days
 DEVICE_EXPIRATION_TIME = 15
 
-SENSORS_DOMAIN = 'binary_sensor'
+SENSORS_DOMAIN = "binary_sensor"
 
 CRITICAL_SENSORS_DEVICE_ENUMS = [
     BinarySensorDeviceClass.CO,

8ef04a70c015f4a67697d6dbe0984673e1d3a8f9
Artem Bezborodko <artem.bezborodko@atisgroup.ru>
2024-06-05 00:31:28 +0300

    Minor refactor.

--- custom_components/domika/entity/router.py
+++ custom_components/domika/entity/router.py
@@ -9,36 +9,40 @@ Author(s): Michael Bogorad
 """
 
 import logging
-from typing import Any
+from typing import Any, cast
 
 import voluptuous as vol
 from homeassistant.components.websocket_api.connection import ActiveConnection
 from homeassistant.components.websocket_api.decorators import websocket_command
 from homeassistant.core import HomeAssistant, callback
 
-from .service import get
 from ..const import MAIN_LOGGER_NAME
+from .service import get
 
 LOGGER = logging.getLogger(MAIN_LOGGER_NAME)
 
 
 @websocket_command(
     {
-        vol.Required("type"): "domika/entity_list",
-        vol.Required("domains"): list,
-    }
+        vol.Required('type'): 'domika/entity_list',
+        vol.Required('domains'): list,
+    },
 )
 @callback
 def websocket_domika_entity_list(
-        hass: HomeAssistant,
-        connection: ActiveConnection,
-        msg: dict[str, Any],
+    hass: HomeAssistant,
+    connection: ActiveConnection,
+    msg: dict[str, Any],
 ) -> None:
     """Handle domika entity_list request."""
-    msg_id = msg.get("id")
-    LOGGER.debug(f'Got websocket message "entity_list", data: %s', msg)
+    msg_id = msg.get('id')
+    if not msg_id:
+        LOGGER.error('Got websocket message "entity_list", msg_id is missing.')
+        return
 
-    domains_list = msg.get("domains")
+    LOGGER.debug('Got websocket message "entity_list", data: %s', msg)
+
+    domains_list = cast(list, msg.get('domains'))
     entities = get(hass, domains_list)
     result = entities.to_dict()
 

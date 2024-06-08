a6789500e300abec8b3f15dc7e1cf886160e3570
Alex Babaev <alex@devpocket.com>
2024-06-08 14:22:46 -0400

    Added capabilities to the entity_list response.

--- custom_components/domika/entity/service.py
+++ custom_components/domika/entity/service.py
@@ -7,8 +7,11 @@ Entity.
 
 Author(s): Michael Bogorad
 """
-
+from homeassistant.components.binary_sensor import BinarySensorDeviceClass
+from homeassistant.components.climate import ClimateEntityFeature
+from homeassistant.components.light import ColorMode, get_supported_color_modes
 from homeassistant.components.search import ItemType, Searcher
+from homeassistant.components.sensor import SensorDeviceClass
 from homeassistant.const import ATTR_DEVICE_CLASS, ATTR_FRIENDLY_NAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import (
@@ -41,35 +44,65 @@ def get(hass: HomeAssistant, domains: list) -> DomikaEntitiesList:
         result.entities[entity_id] = {}
         result.entities[entity_id]["name"] = state.attributes.get(ATTR_FRIENDLY_NAME) or state.name
 
+        # Find out the capabilities of the entity, to be able to select widget size appropriately
+        if state.domain == "light":
+            capabilities = set()
+            supported_modes = get_supported_color_modes(hass, entity_id)
+            if ColorMode.COLOR_TEMP in supported_modes:
+                capabilities.add("brightness")
+                capabilities.add("colorTemperature")
+            if ColorMode.BRIGHTNESS in supported_modes:
+                capabilities.add("brightness")
+            if ((ColorMode.RGB in supported_modes) or
+                    (ColorMode.HS in supported_modes) or
+                    (ColorMode.RGBW in supported_modes) or
+                    (ColorMode.RGBWW in supported_modes) or
+                    (ColorMode.XY in supported_modes)):
+                capabilities.add("brightness")
+                capabilities.add("color")
+
+            result.entities[entity_id]["capabilities"] = capabilities
+        elif state.domain == "climate":
+            capabilities = set()
+            supported_features = hass_entity.get_supported_features(hass, entity_id)
+            if supported_features & ClimateEntityFeature.TARGET_TEMPERATURE:
+                capabilities.add("temperature")
+            if supported_features & ClimateEntityFeature.TARGET_TEMPERATURE_RANGE:
+                capabilities.add("temperatureRange")
+            if supported_features & ClimateEntityFeature.TARGET_HUMIDITY:
+                capabilities.add("humidity")
+
+            result.entities[entity_id]["capabilities"] = capabilities
+
+        # Find out related entity ids, they will be used in the widget
         related_ids = {}
-        if entity_id.startswith("lock."):
+        if state.domain == "lock":
             for related_id in _related(hass, entity_id):
                 state = hass.states.get(related_id)
                 if not state:
                     continue
 
-                if (ATTR_DEVICE_CLASS in state.attributes) and (
+                if (state.domain in ["binary_sensor", "sensor"]) and (ATTR_DEVICE_CLASS in state.attributes) and (
                     state.attributes[ATTR_DEVICE_CLASS]
                     in [
-                        "door",
-                        "garage_door",
-                        "window",
-                        "battery",
+                        BinarySensorDeviceClass.DOOR,
+                        BinarySensorDeviceClass.GARAGE_DOOR,
+                        BinarySensorDeviceClass.WINDOW,
                     ]  # TODO: replace with enum values
                 ):
                     related_ids[state.attributes[ATTR_DEVICE_CLASS]] = related_id
-        elif entity_id.startswith("climate."):
+        elif state.domain == "climate":
             for related_id in _related(hass, entity_id):
                 state = hass.states.get(related_id)
                 if not state:
                     continue
 
-                if (ATTR_DEVICE_CLASS in state.attributes) and (
+                if (state.domain in ["sensor"]) and (ATTR_DEVICE_CLASS in state.attributes) and (
                     state.attributes[ATTR_DEVICE_CLASS]
                     in [
-                        "temperature",
-                        "humidity",
-                    ]  # TODO: replace with enum values
+                        SensorDeviceClass.TEMPERATURE,
+                        SensorDeviceClass.HUMIDITY,
+                    ]
                 ):
                     related_ids[state.attributes[ATTR_DEVICE_CLASS]] = related_id
 

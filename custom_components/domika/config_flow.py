4890464715f08148812acbbdba2f63ed17df7da5
Alex Babaev <alex@devpocket.com>
2024-08-18 19:58:58 -0400

    Added configuration
    Moved translations to “translations” instead of strings.json

--- custom_components/domika/config_flow.py
+++ custom_components/domika/config_flow.py
@@ -1,20 +1,65 @@
 """Config flow for Domika integration."""
 
 import voluptuous as vol
-from homeassistant.config_entries import ConfigFlow
+from typing import Any
+from homeassistant.core import callback
+from homeassistant.helpers import selector
+from homeassistant.config_entries import ConfigEntry, ConfigFlow, OptionsFlow
 from homeassistant.data_entry_flow import FlowResult
 
 from .const import DOMAIN
 
-DOMIKA_SCHEMA = vol.Schema({})
+DOMIKA_SCHEMA = vol.Schema({
+})
 
 
 class DomikaConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Domika."""
 
-    VERSION = 0
-    MINOR_VERSION = 1
+    VERSION = 1
+    MINOR_VERSION = 0
 
     async def async_step_user(self, _user_input: dict[str, str] | None = None) -> FlowResult:
         """Handle a flow initialized by the user."""
         return self.async_create_entry(title=DOMAIN, data={})
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: ConfigEntry,
+    ) -> OptionsFlow:
+        """Create the options flow."""
+        return OptionsFlowHandler(config_entry)
+
+
+class OptionsFlowHandler(OptionsFlow):
+    def __init__(self, config_entry: ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Manage the options."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        entity_selector = selector.selector(
+            {
+                "entity": {
+                    "domain": "binary_sensor",
+                    "multiple": True
+                }
+            }
+        )
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Optional("critical_included_entity_ids", default=self.config_entry.options.get(
+                        "critical_included_entity_ids",
+                    ),): entity_selector,
+                }
+            ),
+        )
